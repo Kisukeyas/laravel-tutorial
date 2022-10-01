a6a5cb2be5848376853ae12f4ed32287d01dafd3
Kisukeyas <kisuke.yas@gmail.com>
2022-10-01 14:24:18 +0900

    tutorial No6

--- server/routes/web.php
+++ server/routes/web.php
@@ -11,5 +11,11 @@
 |
 */
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+
+//フォルダ作成
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'FolderController@create');
+
+//タスク作成
+Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', 'TaskController@create');
