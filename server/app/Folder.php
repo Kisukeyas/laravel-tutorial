ff9ef8879728ae7396f274198a1991a9c674e35f
Kisukeyas <kisuke.yas@gmail.com>
2022-10-01 13:29:27 +0900

    tutorial No5

--- server/app/Folder.php
+++ server/app/Folder.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Folder extends Model
 {
-    //
+    public function tasks()
+    {
+        // 第一引数が関連するモデル名（名前空間も含む）、第二引数が関連するテーブルが持つ外部キーカラム名、第三引数はモデルに hasMany が定義されている側のテーブルが持つ、外部キーに紐づけられたカラムの名前です。ただ第二引数と第三引数は今回のようにある決まりに沿っている場合は省略できます。その決まりとは、第二引数が テーブル名単数形_id で第三引数が id であることです。
+
+        // つまり、Laravel はフォルダテーブルとタスクテーブルを結びつけるためのカラムまで知っているのでフォルダさえわかればタスクも分かってしまうのです。
+        return $this->hasMany('App\Task', 'folder_id', 'id');
+    }
 }
--- server/routes/web.php
+++ server/routes/web.php
@@ -10,5 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+Route::post('/folders/create', 'FolderController@create');
